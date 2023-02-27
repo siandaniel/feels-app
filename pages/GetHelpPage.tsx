ae5d9ca47abf6b5cfbc7644c819bfb394bd7991b
Stacey <stacey6105@gmail.com>
2023-02-27 16:16:33 +0000

    Initialise socket io client

--- pages/GetHelpPage.tsx
+++ pages/GetHelpPage.tsx
@@ -1,7 +1,9 @@
 import { View, StyleSheet } from "react-native";
 import Chat from "../components/Chat";
+import { socket } from "../utils/socket";
 
 export default function GetHelpPage() {
+  socket.connect()
   return (
     <View style={styles.page}>
       <Chat />
