70f4cd6ba6b560e41d352eab229c6727f83f4e33
Stacey <stacey6105@gmail.com>
2023-02-27 10:54:06 +0000

    Nav added back to app.js

--- App.tsx
+++ App.tsx
@@ -26,8 +26,55 @@ export default function App() {
         <Text>Loading fonts</Text>
       </View>
     );
-
-  return <Login />;
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Insights"
+          screenOptions={({ route }) => ({
+            tabBarBadgeStyle: {
+              backgroundColor: orange,
+              fontSize: 13,
+              textAlign: "center",
+              paddingBottom: 1,
+              color: white,
+              height: 17,
+              width: 14,
+            },
+            headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => {
+              if (route.name === "Get Help") {
+                return (
+                  <Ionicons
+                    name={"chatbubble-outline"}
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === "Profile") {
+                return (
+                  <Ionicons name="ios-person-outline" size={size} color={color} />
+                );
+              } else if (route.name === "Insights") {
+                return (
+                  <MaterialIcons name="insights" size={size} color={color} />
+                );
+              }
+            },
+            tabBarActiveTintColor: blue,
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          <Tab.Screen name="Profile" component={ProfilePage} />
+          <Tab.Screen name="Insights" component={MoodTrackingPage} />
+          <Tab.Screen
+            name="Get Help"
+            component={GetHelpPage}
+            options={{ tabBarBadge: 1 }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+    
 }
 
 const styles = StyleSheet.create({
