afd0f77e5d1d1fb9d20a92acae2aa60975ba5dd8
siandaniel <siandaniel@ymail.com>
2023-02-28 15:20:03 +0000

    set up basic profile page for user

--- pages/ProfilePage.tsx
+++ pages/ProfilePage.tsx
@@ -1,17 +1,79 @@
-import { View, Text, StyleSheet } from "react-native";
-import { blue } from "../assets/colours";
+import { View, Text, StyleSheet, Image, Button, Pressable } from "react-native";
+import { blue, white, orange } from "../assets/colours";
+import { LoggedInUserContext } from "../contexts/LoggedInUser";
+import { loggedInUser } from "../types";
+import { getUser, getUserMoods } from "../utils/utils";
+import { todaysDate } from "../utils/todaysDate";
+import {
+  useState,
+  useEffect,
+  useContext,
+  SetStateAction,
+  Dispatch,
+} from "react";
 
 export default function ProfilePage() {
+  const loggedInUserState = useContext(LoggedInUserContext);
+  let loggedInUser: loggedInUser | null = null;
+  let setLoggedInUser: Dispatch<SetStateAction<loggedInUser | null>>;
+
+  if (loggedInUserState !== null) {
+    loggedInUser = loggedInUserState.loggedInUser;
+    setLoggedInUser = loggedInUserState.setLoggedInUser;
+  }
+
+  if (loggedInUser === null) {
+    return null;
+  }
+
   return (
     <View style={styles.page}>
-      <Text> Hello from Profile page!</Text>
+      <Text style={styles.hello}> Hello, {loggedInUser.username}!</Text>
+      <Image source={{ uri: loggedInUser.avatar_url }} style={styles.image} />
+      <Text style={styles.text}>Email: {loggedInUser.email}</Text>
+      <Text style={styles.text}>Member Since: {loggedInUser.date_joined}</Text>
+      {/* <Text style={styles.text}>Last Active: {loggedInUser.updatedAt}</Text> */}
+      <Pressable style={styles.logout} onPress={() => setLoggedInUser(null)}>
+        <Text style={styles.logoutText}>Log Out</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  hello: {
+    marginBottom: 20,
+    color: white,
+    fontSize: 30,
+  },
+  image: {
+    resizeMode: "cover",
+    height: 230,
+    width: 230,
+    borderRadius: 150,
+    marginBottom: 30,
+  },
+  logout: {
+    backgroundColor: orange,
+    height: 30,
+    width: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    marginTop: 50,
+  },
+  logoutText: {
+    color: white,
+  },
   page: {
     backgroundColor: blue,
-    marginTop: 60,
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: white,
+    fontSize: 20,
+    marginBottom: 20,
   },
 });
